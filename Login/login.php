719da699d7955a4f8c1a928b0c412967f34d4a18
trashieeeee <porkolab.martin@szikszi-ozd.hu>
2024-09-15 15:12:12 +0200

    login from database 0.1

---
+++ Login/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <form method="post" action="login.php">
+        <h1>Belépés</h1>
+        <div class="loginBox">
+            <input type="text" placeholder="username" name="username">
+        </div>
+        <div class="loginBox">
+            <input type="password" placeholder="password" name="password">
+        </div>
+        <input type="submit" value="Login" class="loginBtn" name="login-Btn">
+        <div class="signUp">
+            Nincs még fiókod? </br>
+            <a href="#">Regisztráció</a>
+        </div>
+    </form>
+    
+</body>
+</html>
+<?php
+$con=mysqli_connect("localhost","root","");
+if(isset($_POST['login_Btn'])){
+    $username=$_POST ['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * from websitelogin.logindetails WHERE username = '$username'";
+    $result = mysqli_query($con,$sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $resultPassword = $row['password'];
+        if($password == $resultPassword){
+            header('Location:index.html');
+        }else{
+            echo "<script>
+                alert('Login unuccesful');
+                </script>";
+        }
+    }
+}
+
+
+?>
