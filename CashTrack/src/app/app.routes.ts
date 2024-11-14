b35656dc8b67a7e0c6c516e276757e2aacbb951b
trashieeeee <77439099+trashieeeee@users.noreply.github.com>
2024-11-14 13:59:54 +0100

    school

--- CashTrack/src/app/app.routes.ts
+++ CashTrack/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { FelhasznalokComponent } from './components/felhasznalok/felhasznalok.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {
-        path:'',component:FelhasznalokComponent, 
+        path:'',component:FelhasznalokComponent
+    },
+    {
+        path:'login', component:LoginComponent
     }
+
+    
 ];
