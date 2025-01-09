7fa288c03099c03f2861a30b670b5b2e9a963bc0
hatarvedo <77439470+hatarvedo@users.noreply.github.com>
2025-01-09 19:37:13 +0100

    chart package added, Dashboard update 0109.0.4

--- CashTrack/src/app/components/dashboard/dashboard.component.ts
+++ CashTrack/src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,16 @@
 import { Component,OnInit } from '@angular/core';
 import { DataManagerService } from '../../services/data-manager.service';
+import { CommonModule } from '@angular/common'; //
 import { NgFor } from '@angular/common';
 import { HttpClient} from '@angular/common/http';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true, 
-  imports: [NgFor],
+  imports: [NgFor, NgApexchartsModule, CommonModule],
   providers: [DataManagerService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
@@ -16,7 +19,9 @@ export class DashboardComponent {
   kiadasok = [];
   havikoltseg = 0;
   user = JSON.parse(localStorage.getItem('felhasznalo') || '{}');
-
+  bevetelSum:number = 7612;
+  kiadasSum:number = 3162;
+  EURtoHUF:number = 413.70;
   
 constructor(private http: HttpClient,private dataManagerService:DataManagerService) { }
 
