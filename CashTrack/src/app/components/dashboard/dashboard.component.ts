3ea5720fdcdb69b81656bdd52f5e5784ac6c98f6
trashieeeee <porkolab.martin@szikszi-ozd.hu>
2025-01-12 18:30:45 +0100

    ApexCharts implementáció

--- CashTrack/src/app/components/dashboard/dashboard.component.ts
+++ CashTrack/src/app/components/dashboard/dashboard.component.ts
@@ -1,22 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common'; // Importáld a CommonModule-t
 import { DataManagerService } from '../../services/data-manager.service';
-import { NgFor } from '@angular/common';
 import { NgApexchartsModule } from 'ng-apexcharts';
-import { FormsModule } from '@angular/forms'; // Importáld a FormsModule-t
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Importáld a FormsModule-t
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexYAxis,
+  ApexTitleSubtitle,
+  ApexLegend,
+ 
+} from "ng-apexcharts";
+
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  yaxis: ApexYAxis;
+  title: ApexTitleSubtitle;
+  labels: string[];
+  legend: ApexLegend;
+  subtitle: ApexTitleSubtitle;
+};
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, NgFor, NgApexchartsModule, FormsModule], // Add hozzá a CommonModule-t és a FormsModule-t az imports-hoz
+  imports: [CommonModule,  NgApexchartsModule, FormsModule, ReactiveFormsModule], // Add hozzá a CommonModule-t és a FormsModule-t az imports-hoz
   providers: [DataManagerService],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions?: Partial<ChartOptions>;
+  constructor(private http: HttpClient, private dataManagerService: DataManagerService, private router:Router,private authService:AuthService){
+    const series = {
+      monthDataSeries1: {
+        prices: [10, 41, 35, 51, 49, 62, 69, 91, 126],
+        dates: []
+      }
+    };
+
+    this.chartOptions = {
+      series: [
+        {
+          name: "Kiadások",
+          data: series.monthDataSeries1.prices
+        }
+      ],
+      chart: {
+        type: "area",
+        height: 350,
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth"
+      },
+
+      title: {
+        text: "Havi kiadások",
+        align: "left"
+      },
+      subtitle: {
+        text: "Költségek",
+        align: "left"
+      },
+      labels: series.monthDataSeries1.dates,
+      xaxis: {
+        type: "datetime"
+      },
+      yaxis: {
+        opposite: true
+      },
+      legend: {
+        horizontalAlign: "left"
+      }
+    };
+
+  }
+  //ApexCharts
+
+
+
   kiadasok = [];
   havikoltseg = 0;
   user = JSON.parse(localStorage.getItem('felhasznalo') || '{}');
@@ -28,7 +111,7 @@ export class DashboardComponent implements OnInit {
   currentYear: number = 0; // Új változó az aktuális évhez
   currentMonth: number = 0; // Új változó az aktuális hónaphoz
 
-  constructor(private http: HttpClient, private dataManagerService: DataManagerService, private router:Router,private authService:AuthService) { }
+
 
   ngOnInit(): void {
     this.dataManagerService.havikiadasok().subscribe((data) => {
